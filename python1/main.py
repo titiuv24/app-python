b24f244a4e4cf100b537555dc808025ea0cc3781
Thibaud Melano <thibaudmelano@gmail.com>
2024-03-27 09:04:28 +0000

    variables + input, display du print

--- python1/main.py
+++ python1/main.py
@@ -1,1 +1,15 @@
 print("hello world")
+a = 6
+b = 3
+a /= 2 * b
+#print(a)
+#anything = input("Tell me anything...\n")
+#print("Hmm...", anything, "... Really?")
+#anything2 = int(input("Tell me anything...\n"))
+#print(anything2*2)
+
+print("+" + 10 * "-" + "+")
+print(("|" + " " * 10 + "|\n") * 3, end="")
+print("|" + " " * 2 + "coucou" + 2 * " " + "|")
+print(("|" + " " * 10 + "|\n") * 3, end="")
+print("+" + 10 * "-" + "+")
